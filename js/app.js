ffc26e64276ada6249b23360442f8c452c63520a
aungureanu614 <aungureanu614@gmail.com>
2016-07-22 11:41:58 -0700

    js/

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 var randomNum;
 var count = 0;
-var str = " ";
-var listItem;
-var userGuess;
-var userGuessArray = [];
+var str;
+
 
 
 $(document).ready(function() {
@@ -18,14 +16,14 @@ $(document).ready(function() {
     $("a.close").click(function() {
         $(".overlay").fadeOut(1000);
     });
-
+    //start new game when user clicks new game
     $("a.new").click(function() {
         newGame();
 
     });
     $("#guessButton").click(function(e) {
         userInput();
-        e.preventDefault();
+        e.preventDefault(); //prevent page from reloading on button click
 
     });
 
@@ -34,30 +32,32 @@ $(document).ready(function() {
 });
 
 function newGame() {
+    //clears and sets all fields to default for a new game
     $('#guessList').empty();
     $('#feedback').text("Make your Guess!");
     $('#userGuess').val('');
     $('#count').empty().append(count = 0);
-    randomNum = Math.floor(Math.random() * (100 - 1) + 1);
-    $("#guessButton").prop("disabled", false);
+    randomNum = Math.floor(Math.random() * (100 - 1) + 1); //generate a random number between 1 and 100
+    $("#guessButton").prop("disabled", false); //re-enable the guess button after last game
 
 
 }
 
-function userInput() {
-    userGuess = +($('#userGuess').val());
+function userInput() { 
+    //gets user guess and sends it to the userFeedback function
+    userGuess = +($('#userGuess').val()); 
     userFeedback(userGuess);
 
 }
 
 function userFeedback(userNum) {
-
+    //check if user's guess matches the random number
     if (userNum === randomNum) {
         str = "You got it!";
         displayOnScreen(str, userNum);
-        $("#guessButton").prop("disabled", true);
+        $("#guessButton").prop("disabled", true); //disable guess button after the user has guessed correctly
     } else {
-
+        //check guesses and alert the user
         switch (userNum !== randomNum) {
             case (userNum > 100 || userNum < 1):
                 alert("Please choose a number between 1 and 100");
